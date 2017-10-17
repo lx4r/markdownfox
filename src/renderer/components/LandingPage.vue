e8e832b0c9c007fddb0b54b48f89f23e55ec5cd0
lx4r <hallo@l3r.de>
2017-10-17 20:28:12 +0200

    fixing e2e test for app name, moving functions to components methods attribute, separate function for view update to (hopefully) be able to test without IPC messages

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -22,54 +22,56 @@
 
   let currentFilePath = null
 
-  let mainData = {
-    renderedMD: '',
-    fileOpen: false
-  }
-
-  ipcRenderer.on('file-contents', (event, filePath, data) => {
-    if (currentFilePath === filePath) {
-      // file was updated
-      mainData.renderedMD = marked(data)
-    } else {
-      // a new file was loaded
-      mainData.fileOpen = true
-
-      // build modified image render method to fix image path
-      const dirPath = path.dirname(filePath)
-      const newRenderer = new marked.Renderer()
-      newRenderer.image = function (href, title, text) {
-        const modifiedHref = path.resolve(dirPath, href)
-        let out = '<img src="' + modifiedHref + '" alt="' + text + '"'
-        if (title) {
-          out += ' title="' + title + '"'
-        }
-        out += this.options.xhtml ? '/>' : '>'
-        return out
-      }
-      // enable highlighting and set modified renderer
-      // this is done for every file as the image render method has to be adapted to the file path
-      marked.setOptions({
-        highlight: function (code) {
-          return require('highlight.js').highlightAuto(code).value
-        },
-        renderer: newRenderer
-      })
-
-      mainData.renderedMD = marked(data)
-    }
-  })
-
   export default {
     name: 'landing-page',
     components: {Licenses, ErrorModal},
+    mounted: function () {
+      ipcRenderer.on('file-contents', (event, filePath, data) => {
+        this.updateView(filePath, data)
+      })
+    },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
+      },
+      updateView (filePath, newData) {
+        if (currentFilePath === filePath) {
+          // file was updated
+          this.renderedMD = marked(newData)
+        } else {
+          // a new file was loaded
+          this.fileOpen = true
+
+          // build modified image render method to fix image path
+          const dirPath = path.dirname(filePath)
+          const newRenderer = new marked.Renderer()
+          newRenderer.image = function (href, title, text) {
+            const modifiedHref = path.resolve(dirPath, href)
+            let out = '<img src="' + modifiedHref + '" alt="' + text + '"'
+            if (title) {
+              out += ' title="' + title + '"'
+            }
+            out += this.options.xhtml ? '/>' : '>'
+            return out
+          }
+          // enable highlighting and set modified renderer
+          // this is done for every file as the image render method has to be adapted to the file path
+          marked.setOptions({
+            highlight: function (code) {
+              return require('highlight.js').highlightAuto(code).value
+            },
+            renderer: newRenderer
+          })
+
+          this.renderedMD = marked(newData)
+        }
       }
     },
     data: function () {
-      return mainData
+      return {
+        renderedMD: '',
+        fileOpen: false
+      }
     }
   }
 </script>
